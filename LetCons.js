3d5ee4899ed8b94994a31a66020d309dfbb011b9
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-06-16 13:02:27 -0500

    flecha

--- LetCons.js
+++ LetCons.js
@@ -5,13 +5,13 @@ console.log(edad);
 let funcion = function duplicar(valor)
 {
     return valor * 2;
-}
+};
 
 const persona = {
     nombre: 'Daniel',
     apellido: 'Ramirez',
     fechaNacimiento: new Date()
-}
+};
 
 persona.nombre = 'Alejandro'; //no hay problema pq no estoy reasignando neuvamente el objeto
 
---
+++ flecha.js
@@ -0,0 +1,9 @@
+let funcion = function duplicar(valor){
+    return valor * 2;
+};
+
+let duplica = (valor) => {
+    return valor * 2;
+}
+
+let duplicaDos = valor => valor * 2;
