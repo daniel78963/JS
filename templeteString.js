11ffc9dfe30594645e5fd18e629a6714e1dc583a
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-06-21 19:58:54 -0500

    ternario

--- templeteString.js
+++ templeteString.js
@@ -12,3 +12,13 @@ const saludoTemplate = `Hola, ${apellido}, ¿Cómo estás?`;
 console.log(saludoTemplate);
 
 //node templeteString.js
+
+let sumar = (a,b) => a+b;
+
+const mensaje = `Hola ${apellido},
+
+Esta es una carta desde ${pais}.
+La suma de de 2 y 3 es ${sumar(2,3)}
+`;
+
+console.log(mensaje);
---
+++ ternario.js
@@ -0,0 +1,5 @@
+const edad = 16;
+let mensaje = (edad >= 18) ? "Puedes pasar": "No puedes pasar";
+
+console.log(mensaje);
+
