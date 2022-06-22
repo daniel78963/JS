fe5e15f6e1cb29a79bdc0136c3c1a194c378e171
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-06-21 20:37:13 -0500

    notacion de objetos aumentada

--- ternario.js
+++ ternario.js
@@ -3,3 +3,16 @@ let mensaje = (edad >= 18) ? "Puedes pasar": "No puedes pasar";
 
 console.log(mensaje);
 
+const paisProp = 'pais';
+const paisValor = 'CO';
+
+let persona = {
+    nombre: "Daniel",
+    [paisProp]: paisValor
+}
+
+console.log(persona);
+
+const retornarValoresPersona =  (prop) => persona[prop];
+console.log(retornarValoresPersona('nombre'));
+console.log(persona['pais']);
