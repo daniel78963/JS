9f0778b9bb0a0bdf6014599d93fe40abba31ad2f
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-06-23 15:07:29 -0500

    spread 2

--- spread.js
+++ spread.js
@@ -9,3 +9,25 @@ console.log(masNumeros);
 const masNumerosMismo = [1, ...numeros];
 console.log(masNumerosMismo);
 
+const otrosNumeros = [4,5];
+const concatenar = [...numeros, ...otrosNumeros];
+console.log(concatenar);
+
+const [primero, ...resto] = masNumerosMismo;
+console.log('masNumeros', masNumerosMismo);
+console.log('primero', primero);
+console.log('resto', resto);
+
+//clonar
+const masNumerosMismoClonado = [...masNumerosMismo];
+
+const persona = {
+    nombre: "Daniel",
+    apellido: "Ramirez"
+}
+
+const edadPersona = {
+    ...persona,
+    edad: 22
+}
+console.log('personaEdad', edadPersona);
