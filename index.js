2c5ab4e82b5b6dc6de3ce7e35bf6bb26c6d76df6
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-07-02 20:01:26 -0500

    hola mundo

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-import {nombre} from './persona'
+import { nombre } from './persona.js'
 
-document.getElementById("app").innerHTML = `
-${nombre}
-`;
+// document.getElementById('app').innerHTML = "fff"; 
+
+// document.getElementById("app").innerHTML = `
+// ${nombre}
+// `;
+const mi = document.getElementById('app');
+mi.innerText = '¡Hola mundo!';
+
+const miTitulo = document.querySelector('h1');
+miTitulo.textContent = '¡Hola mundo!';
