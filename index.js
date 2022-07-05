f95e1c04aa56b6484e81ad4f27be3a44d6357b65
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-07-05 16:09:21 -0500

    export functions

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import { nombre } from './persona.js'
+// import { nombre, edad } from './persona'
+// import nombre, { edad } from './persona'
+import nombre, { edad as edad2, duplicar } from './persona'
 
 // document.getElementById('app').innerHTML = "fff"; 
 
 // document.getElementById("app").innerHTML = `
 // ${nombre}
+// `;
+
+// document.getElementById("app").innerHTML = `
+// ${nombre} ${edad2} ${duplicar(3)}
 // `;
 const mi = document.getElementById('app');
 mi.innerText = '¡Hola mundo!';
--- persona.js
+++ persona.js
@@ -2,4 +2,5 @@ const nombre = "Daniel Ramirez";
 const edad = 20;
 const duplicar = (valor) => valor * 2;
 
-export {nombre, edad};
+// export {nombre, edad};
+export {nombre as default, edad, duplicar};
